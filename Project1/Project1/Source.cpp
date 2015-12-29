80f57fdf2b998f1d10502d0a9dff09ab72551706
eladwf <eladwf@gmail.com>
2015-12-29 10:57:12 +0200

    tests

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -6,5 +6,5 @@ void main()
 	cout << "hi world" << endl;
 	cout<<"hello form git"<<endl;
 	cout << "hello form elad" << endl;
-	if(1) cout << "pull request" << endl;
+	if(1) cout << "pull request" << endl; //test
 }
